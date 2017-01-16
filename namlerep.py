790e12b24edece169223e00900abc40ac208fa63
riccitensor <michael@deckard.tech>
2017-01-16 23:38:41 +0100

    Primax rep

---
+++ namlerep.py
@@ -0,0 +1,40 @@
+# Copyright (c) 2016-present, Deckard, SoCrowd GmbH.
+# All rights reserved.
+
+# Represent a number as a primax.
+# Example of a primax:
+#
+#   20992335 = 3^1*5^1*7^2*13^4
+#   Primax(20992335) =
+#   3 5 7 13
+#   1 1 2 4
+
+import functools as ft
+
+def getFactors(n):
+    return set(ft.reduce(list.__add__,
+                ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+
+def getPrimax(n):
+    ret= {}
+    primfac = []
+    d = 2
+
+    while d*d <= n:
+        while (n % d) == 0:
+            primfac.append(d)
+            n //= d
+        d += 1
+    if n > 1:
+       primfac.append(n)
+
+    uniqueVals = set(primfac)
+
+    for i in uniqueVals:
+        ret[i] = 0
+    for i in primfac:
+        ret[i] = ret[i] + 1
+        
+    return ret
+
+print(getPrimax(20992335))
