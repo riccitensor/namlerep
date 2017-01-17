24935de071dee84030e4314bea600b2698b1a353
riccitensor <michael@deckard.tech>
2017-01-17 10:25:30 +0100

    Starting NN and reading data from images

---
+++ nn.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2016-present, Deckard, SoCrowd GmbH.
+# All rights reserved.
+
+# Neural network
+
+import numpy as np
+import 
+
+# simple network by http://iamtrask.github.io/2015/07/12/basic-python-network/
+X = np.array([ [0,0,1],[0,1,1],[1,0,1],[1,1,1],[1,1,0] ])
+y = np.array([[0,1,1,0,1]]).T
+syn0 = 2*np.random.random((3,4)) - 1
+syn1 = 2*np.random.random((4,1)) - 1
+for j in range(60000):
+    l1 = 1/(1+np.exp(-(np.dot(X,syn0))))
+    l2 = 1/(1+np.exp(-(np.dot(l1,syn1))))
+    l2_delta = (y - l2)*(l2*(1-l2))
+    l1_delta = l2_delta.dot(syn1.T) * (l1 * (1-l1))
+    syn1 += l1.T.dot(l2_delta)
+    syn0 += X.T.dot(l1_delta)
+
+print (l1)
---
+++ readImage.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2016-present, Deckard, SoCrowd GmbH.
+# All rights reserved.
+
+# Read images
+
+from PIL import Image
+
+def readImage(filename):
+    im = Image.open(filename)
+    pix = im.load()
+    width = im.size[0]
+    height = im.size[1]
+    return (pix,width,height)
+
+img = readImage("data/pic_small.jpg")
+print(img)
