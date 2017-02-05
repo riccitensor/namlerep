e7d2276eb2946bd8764f5350ae9668f019c9f051
riccitensor <michael@deckard.tech>
2017-02-05 15:42:20 +0100

    First version of the encoding + turned into pics

--- encoder.py
+++ encoder.py
@@ -4,6 +4,10 @@
 # Read images
 
 from PIL import Image
+import math
+
+def sigmoid(x):
+  return 1 / (1 + math.exp(-x))
 
 def readImage(filename):
     im = Image.open(filename)
@@ -12,14 +16,22 @@ def readImage(filename):
     height = im.size[1]
     return (pix,width,height)
 
-def encodePrimax(filename):
+def primeLexEncode(prime,lex):
+    return ( (prime*lex) % 255, (prime*lex) % 255, (prime*lex) % 255)
+
+def encodePrimax(arr,filename):
     img = Image.new( 'RGB', (255,255), "black")
     pixels = img.load()
 
     for i in range(img.size[0]):
-        for j in range(img.size[1]):
-            pixels[i,j] = (i, j, 33)
+        if (i in arr.keys()):
+            for j in range(img.size[1]):
+                pixels[i,j] = primeLexEncode(i,arr[i])
+                print(((50 * (i+1))*arr[i]) % 255)
+        else:
+            for j in range(img.size[1]):
+                pixels[i,j] = (0,0,0)
     img.save(filename)
     img.show()
 
-encodePrimax("dupa.bmp")
+#maxKey = max(test_array.keys(), key=int)
--- namlerep.py
+++ namlerep.py
@@ -10,6 +10,16 @@
 #   1 1 2 4
 
 import functools as ft
+import encoder as enc
+
+def get_primes(n):
+    numbers = set(range(n, 1, -1))
+    primes = []
+    while numbers:
+        p = numbers.pop()
+        primes.append(p)
+        numbers.difference_update(set(range(p*2, n+1, p)))
+    return primes
 
 def getPrimax(n):
     ret= {}
@@ -34,3 +44,10 @@ def getPrimax(n):
     return ret
 
 print(getPrimax(20992335))
+
+#test for a list of primes
+for i in get_primes(5):
+    enc.encodePrimax(getPrimax(i), str(i) + ".bmp")
+
+for i in range(0,10):
+    enc.encodePrimax(getPrimax(i), str(i) + ".bmp")
